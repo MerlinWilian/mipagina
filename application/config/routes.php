cfdd7a2401013a3226a62d5a3662a69f74cef51c
MerlinWilian <mwgg92@gmail.com>
2017-06-28 22:40:10 -0600

    I have ordered Header, Footer and CSS files

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'welcome';
 $route['home'] = 'home';
+$route['anotherAction'] = 'anotherAction';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/views/anotherAction/anotherAction.php
@@ -0,0 +1,7 @@
+<?php $this->load->view('template/header'); ?>
+
+<div class="container marketing">
+anotherAction
+</div>
+
+<?php $this->load->view('template/footer'); ?>
